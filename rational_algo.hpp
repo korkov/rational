d830dd62fb54fad4423eacdef949d0e89b04a76f
Alexander Korkov <korkov@yandex.ru>
2011-08-02 18:46:26 +0400

    - fixed Newton algorithm # thanks to http://f0rbidik.habrahabr.ru/

--- rational_algo.hpp
+++ rational_algo.hpp
@@ -7,26 +7,13 @@ namespace rational
 {
   namespace detail
   {
-    template <bool n, class A1, class A2>
-    struct meta_if
-    {
-      typedef A2 type;
-    };
-
-    template <class A1, class A2>
-    struct meta_if<true, A1, A2>
-    {
-      typedef A1 type;
-    };
-
     template <int64_t p, class res, class x>
     struct sqrt_eval
     {
       typedef typename divide<x, res>::type t1;
       typedef typename plus<res, t1>::type t2;
       typedef typename divide<t2, rational_t<2,1> >::type tmp;
-      typedef typename meta_if<less<tmp, res>::value, tmp, res>::type less_val;
-      typedef typename sqrt_eval<p-1, less_val, x>::type type;
+      typedef typename sqrt_eval<p-1, tmp, x>::type type;
     };
 
     template <class res, class x>
