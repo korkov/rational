2651944e8611c803a8e13b9fb03f000d905104e9
Alexander Korkov <korkov@yandex.ru>
2011-08-03 13:09:18 +0400

    # fixed sqrt

--- rational/math.hpp
+++ rational/math.hpp
@@ -11,33 +11,25 @@ namespace rational
     namespace mpl = boost::mpl;
     using namespace mpl::placeholders;
 
-    
-    struct sqrt_eval
+    struct sqrt
     {
-      template <class p, class res, class x>
+      template <
+        class x,
+        class p = mpl::int_<15>,
+        class res = mpl::apply<divide, mpl::apply<plus, x, rational_t<1,1> >, rational_t<2,1> >
+        >
       struct apply
       {
         typedef typename mpl::apply<divide, x, res>::type t1;
         typedef typename mpl::apply<plus, res, t1>::type t2;
         typedef typename mpl::apply<divide, t2, rational_t<2,1> >::type tmp;
-        typedef typename mpl::apply<sqrt_eval, mpl::int_<p::value-1>, tmp, x>::type type;
+        typedef typename mpl::apply<sqrt, x, mpl::int_<p::value-1>, tmp>::type type;
       };
 
-      template <class res, class x>
-      struct apply<mpl::int_<0>, res, x>
+      template <class x, class res>
+      struct apply<x, mpl::int_<0>, res>
       {
         typedef typename res::type type;
-      };
-    };
-
-    struct sqrt
-    {
-      template <class x>
-      struct apply
-      {
-        typedef typename mpl::apply<plus, x, rational_t<1,1> >::type tmp;
-        typedef typename mpl::apply<divide, tmp, rational_t<2,1> >::type res;
-        typedef typename mpl::apply<sqrt_eval, mpl::int_<15>, res, x>::type type;
       };
 
       template <int64_t a>
